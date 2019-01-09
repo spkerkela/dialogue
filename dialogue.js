c995f29ede290c523adeba9321a19353eaa94de9
Simo-Pekka Kerkelä <simopekka1990@gmail.com>
2019-01-09 18:53:22 +0200

    add some logic to options

--- dialogue.js
+++ dialogue.js
@@ -1,6 +1,18 @@
 var dialogue = document.getElementById("dialogue");
 var optionsContainer = document.getElementById("options");
 
+var gameState = {
+  playerHasGem: false
+};
+
+var calledNTimes = function(n) {
+  var called = 0;
+  return function() {
+    called++;
+    return called >= n;
+  };
+};
+
 var dialogueTree = [
   {
     id: "root",
@@ -8,14 +20,35 @@ var dialogueTree = [
     options: [
       { optionText: "Hello", leadsTo: "hub" },
       { optionText: "How are you", leadsTo: "well" },
+
       { optionText: "Goodbye", leadsTo: "bye" }
     ]
+  },
+  {
+    id: "give-gem",
+    dialogue: "Sure, here you go!",
+    options: [{ optionText: "Thanks!", leadsTo: "hub" }]
   },
   {
     id: "hub",
     dialogue: "Is there something I can help you with?",
     options: [
       { optionText: "How was this page made?", leadsTo: "explain-page" },
+      {
+        optionText: "Have we been talking for long enough?",
+        leadsTo: "alternative-end",
+        requirement: calledNTimes(3)
+      },
+      {
+        optionText: "Can you give me a gem?",
+        leadsTo: "give-gem",
+        requirement: function() {
+          return !gameState.playerHasGem;
+        },
+        callback: function() {
+          gameState.playerHasGem = true;
+        }
+      },
       { optionText: "Nope, bye", leadsTo: "bye" }
     ]
   },
@@ -49,6 +82,11 @@ var dialogueTree = [
     id: "bye",
     dialogue: "Bye",
     options: []
+  },
+  {
+    id: "alternative-end",
+    dialogue: "Yes I believe so. Laters",
+    options: []
   }
 ];
 
@@ -73,10 +111,16 @@ function renderDialogue(id) {
   clearOptions();
   changeDialogue(dialogue.dialogue);
   dialogue.options.forEach(function(option) {
+    if (option.requirement != null && !option.requirement()) {
+      return;
+    }
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(option.optionText));
     li.className = "clickable option";
     li.onclick = function(clicked) {
+      if (option.callback != null) {
+        option.callback(option);
+      }
       renderDialogue(option.leadsTo);
     };
     optionsContainer.appendChild(li);
